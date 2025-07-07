66ccaf7d712638ef80323b38cb38b5b7c7ebc46a
Vessel9817 <151808241+Vessel9817@users.noreply.github.com>
2025-07-06 20:43:25 -0400

    Made installer more robust
    - Added shebang to make explicit the installer executable
    - Replaced apt with apt-get, as it's better suited for CLI use
    - Changed how the installer handles submodules so that it installs all submodules
    - Added git as a dependency in the installer
    
    All tests passed
    
    Signed-off-by: Vessel9817 <151808241+Vessel9817@users.noreply.github.com>

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
-sudo apt update && sudo apt upgrade -y
-sudo apt install astyle cmake gcc ninja-build libssl-dev libsodium-dev python3-pytest python3-pytest-xdist unzip xsltproc doxygen graphviz python3-yaml valgrind -y
-git clone -b main https://github.com/open-quantum-safe/liboqs.git
+#!/usr/bin/env bash
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install -y astyle cmake gcc git ninja-build libssl-dev libsodium-dev python3-pytest python3-pytest-xdist unzip xsltproc doxygen graphviz python3-yaml valgrind
+git submodule update --init
 cd liboqs
 mkdir build && cd build
 cmake -DCMAKE_INSTALL_PREFIX=/usr/local -DOQS_USE_OPENSSL=ON ..
